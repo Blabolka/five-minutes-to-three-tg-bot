5cc5e50a850f67db4fcc8bf614d79e830af9d6ce
Andrii Klochko <andrii.k@aquimarketing.com>
2021-09-15 17:12:27 +0300

    Change limit from 50 MB to 35 MB according to Telegram Bot API documentation

--- src/common/constants/photosToPdf.ts
+++ src/common/constants/photosToPdf.ts
@@ -1,4 +1,4 @@
-const MAX_MEGABYTES = 50
+export const MAX_MEGABYTES = 35
 
-// 50 000 000 bytes = 50 MB
+// 35 000 000 bytes = 35 MB
 export const MAX_FILE_SUMMARY_SIZE = MAX_MEGABYTES * Math.pow(10, 6)
