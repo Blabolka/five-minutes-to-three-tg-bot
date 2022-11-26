ef8c4bcb69d30b8bab39da4e1676bb370ad59474
Andrii Klochko <klochko.andrew.official@gmail.com>
2022-11-26 18:24:32 +0200

    Update dependencies

--- src/common/tools/db.ts
+++ src/common/tools/db.ts
@@ -3,9 +3,6 @@ import mongoose from 'mongoose'
 const connectUri: string = process.env.MONGO_CONNECT_URI || ''
 
 mongoose
-    .connect(connectUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(connectUri)
     .then(() => console.log('Connected to db'))
     .catch((error) => console.error(error))
